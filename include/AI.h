6d68a949d9e5152acfada0fafd2adfbe28d6f9c2
luis <luisdi.28@estudiantec.cr>
2022-10-18 21:54:19 -0600

    Commit implementación comentarios

--- include/AI.h
+++ include/AI.h
@@ -11,24 +11,29 @@ public:
     ~AI();
     bool makeMove(SDL_Event *);
 private:
-    // New AI Stuff
+    // Se encuentra el máximo y el mínimo
     int findMax(int,int);
     int findMin(int,int);
 
+    // Se encuentra el valor máximo y el valor mínimo
     int maxValue(vector<vector<int>> tempBoard, vector<Piece> teamCopy, vector<Piece> enemyTeamCopy, int depth, Directions direction);
     int minValue(vector<vector<int>> tempBoard, vector<Piece> teamCopy, vector<Piece> enemyTeamCopy, int depth, Directions direction);
 
+    // Se encuentra el movimiento mínimo y el movimirnto máximo
     int minMove(vector<vector<int>> tempBoard, vector<Piece> teamCopy, vector<Piece> enemyTeamCopy, int depth);
     int maxMove(vector<vector<int>> tempBoard, vector<Piece> teamCopy, vector<Piece> enemyTeamCopy, int depth);
 
+    // Se chequea el nodo y se calcula el valor
     bool checkNode(vector<vector<int>> tempBoard, vector<Piece> teamCopy, vector<Piece> enemyTeamCopy, Directions direction, bool enemy);
     int valueCalculator(vector<Piece> teamCopy, vector<Piece> enemyTeamCopy);
 
+    // Se actualiza el equipo de la IA
     void updateTeam(vector<vector<int>> &tempBoard, vector<Piece> &teamCopy, bool enemy);
 
+    // Se actualizan las reinas del equipo de la IA
     void updateKings(vector<vector<int>> &tempBoard, vector<Piece> &teamCopy, bool enemy);
 
-    // Old AI stuff
+    //Acá se obtiene el equipo enemigom se cambia la dirección del
     void getEnemyTeam();
     bool changeWithDirection(int&,int&,Directions, bool);
     bool killCheckArea(vector<vector<int>> tempBoard, int,int,Directions, bool);
